3a6ff8769367ab4fc9d769d646d0f4315ca5ddbf
Andrew <ankoz2000@mail.ru>
2021-10-23 21:03:31 +0300

    db change

--- Server/app/database/entities.py
+++ Server/app/database/entities.py
@@ -1,26 +1,40 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
+from app import app
 
+db = SQLAlchemy(app)
 ROLE_USER = 0
+
 
 class User(db.Model):
 
-    id              = db.Column(db.Integer, primary_key = True)
-    login           = db.Column(db.String(64), index = True, unique = True)
-    password        = db.Column(db.String(120), index = True, unique = False)
+    __tablename__ = 'user'
 
-    name            = db.Column(db.String(64), index=True, unique=False)
-    lastName        = db.Column(db.String(64), index=True, unique=False)
-    email           = db.Column(db.String(120), index = True, unique = True)
+    id              = db.Column(db.Integer, index=True, primary_key=True)
+    login           = db.Column(db.String(64), unique = True)
+    password        = db.Column(db.String(120), unique = False)
 
-    age             = db.Column(db.Integer, index=True, unique=False)
-    sex             = db.Column(db.Integer, index=True, unique=False)
-    interests       = db.Column(db.String, index=True, unique=False)
-    routeId         = db.Column(db.Integer)
+    lastName        = db.Column(db.String(64), unique=False)
+    email           = db.Column(db.String(120), unique = True)
+    name            = db.Column(db.String(64), unique=False)
 
-    role            = db.Column(db.SmallInteger, default = ROLE_USER)
+    age             = db.Column(db.Integer, unique=False)
+    sex             = db.Column(db.Integer, unique=False)
+    role            = db.Column(db.SmallInteger, default=ROLE_USER)
+    routeId         = db.Column(db.Integer, index=True)
+    interests       = db.Column(db.String, unique=False)
+
 
     def get_id(self):
         return self.id
 
     def __repr__(self):
         return '<User {}>'.format(self.login)
+
+
+class Route(db.Model):
+    __tablename__ = 'route'
+
+    id              = db.Column(db.Integer, primary_key=True)
+    length          = db.Column(db.Integer, unique=False)
+    isPublic        = db.Column(db.Integer, unique=False)
+    timeElapsed     = db.Column(db.Integer, unique=False)
